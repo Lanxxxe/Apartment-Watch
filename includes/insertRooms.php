557a1da293343942305afaf700bac5b768549329
zooo3y <eloisacabezasdecio@gmail.com>
2024-05-28 15:02:30 +0800

    update

--- includes/insertRooms.php
+++ includes/insertRooms.php
@@ -1,34 +1,40 @@
 <?php
 include './includes/databaseConnection.php';
+
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $RoomNumber = $_POST['roomnumber'];
-    $RoomID = $_POST['roomid'];
+    $RoomIDs = $_POST['roomid'];
     $ApartmentID = $_POST['apartmentid'];
-    $RoomType = $_POST['roomtype'];
-    $RoomStatus = $_POST['roomstatus'];
-    $RentAmount = $_POST['rentamount'];
+    $RoomTypes = $_POST['roomtype'];
+    $RoomStatuses = $_POST['roomstatus'];
+    $RentAmounts = $_POST['rentamount'];
 
-    while ($RoomNumber > 0) {
-        $query = "INSERT INTO rooms_table (Room_ID, Apartment_ID, Room_Type, Room_Status, Rent_Amount) VALUES (:roomID, :apartmentID, :roomType, :roomStatus, :rentAmount)";
+    // Ensure these are arrays
+    if (is_array($RoomIDs) && is_array($RoomTypes) && is_array($RoomStatuses) && is_array($RentAmounts)) {
+        for ($i = 0; $i < $RoomNumber; $i++) {
+            $query = "INSERT INTO rooms_table (Room_ID, Apartment_ID, Room_Type, Room_Status, Rent_Amount) VALUES (:roomID, :apartmentID, :roomType, :roomStatus, :rentAmount)";
 
-        $statement = $PHP_Data_Object->prepare($query);
+            $statement = $PHP_Data_Object->prepare($query);
 
-        $statement->bindParam(':roomID', $RoomID);
-        $statement->bindParam(':apartmentID', $RoomID);
-        $statement->bindParam(':roomType', $RoomID);
-        $statement->bindParam(':roomStatus', $RoomID);
-        $statement->bindParam(':rentAmount', $RentAmount);
+            $statement->bindParam(':roomID', $RoomIDs[$i]);
+            $statement->bindParam(':apartmentID', $ApartmentID);
+            $statement->bindParam(':roomType', $RoomTypes[$i]);
+            $statement->bindParam(':roomStatus', $RoomStatuses[$i]);
+            $statement->bindParam(':rentAmount', $RentAmounts[$i]);
 
-        if ($statement->execute()) {
-            echo 'Success';
+            if ($statement->execute()) {
+                echo 'Success';
+            } else {
+                echo 'Failed to insert room: ' . $RoomIDs[$i];
+            }
         }
+    } else {
+        echo 'Error: Invalid input.';
     }
 
     $PHP_Data_Object = null;
     $statement = null;
-    
+
     header('Location: ./roomStatus.php');
-
-
-
+    exit();
 }
